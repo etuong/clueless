cde284225c1fbed6b1c4784b9ea031ce1d30044f
Ethan Uong <etuong@gmail.com>
2019-10-01 23:06:32 -0400

    Made comments on pub-sub files

--- server/messages/publish.py
+++ server/messages/publish.py
@@ -2,12 +2,20 @@
 import pika
 import sys
 
+# Name of the Exchange
 exchange_name='logs'
 
+# Open a connection with localhost
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+
+# Open a channel with exchange type as fanout
 channel = connection.channel()
 channel.exchange_declare(exchange=exchange_name, exchange_type='fanout')
+
+# Publish a message to the same exchange
 message = ' '.join(sys.argv[1:]) or "info: Hello World!"
 channel.basic_publish(exchange=exchange_name, routing_key='', body=message)
+
+# Log and close connection
 print(" [x] Sent %r" % message)
 connection.close()
--- server/messages/subscribe.py
+++ server/messages/subscribe.py
@@ -1,22 +1,31 @@
 #!/usr/bin/env python
 import pika
 
+# Name of exchange, must be the same as the name in the publisher
 exchange_name='logs'
 
+# Open a connection with localhost
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
 
+# Open a channel with exchange type as fanout
+channel = connection.channel()
 channel.exchange_declare(exchange=exchange_name, exchange_type='fanout')
 
+# Create a queue with a random name, let the server choose a random queue name
+# Exclusive flag is to delete the queue if consumer connection is closed
 result = channel.queue_declare(queue='', exclusive=True)
 queue_name = result.method.queue
 
+# Tell the exchange to send messages to our queue from binding
 channel.queue_bind(exchange=exchange_name, queue=queue_name)
 
+# Logging
 print(' [*] Waiting for logs. To exit press CTRL+C')
 
+# Callback function
 def callback(ch, method, properties, body):
     print(" [x] %r" % body)
 
+# Start consuming messages
 channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
 channel.start_consuming()
